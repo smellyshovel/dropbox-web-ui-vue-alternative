c096701e28449861cad35e1a655a0e88ba14c331
Matthew Mamonov <g.smellyshovel@gmail.com>
2019-02-05 23:38:22 +0300

    deny text selection

--- src/views/FileManager/FileManager.vue
+++ src/views/FileManager/FileManager.vue
@@ -139,5 +139,12 @@ export default {
     height: 100%;
     display: grid;
     grid-template-columns: 300px auto;
+
+    -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+    -khtml-user-select: none; /* Konqueror HTML */
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none;
 }
 </style>
