8b2f6d765f46fcec94d264842ce8ee88e4ec1ed9
Matthew Mamonov <g.smellyshovel@gmail.com>
2019-02-08 00:38:17 +0300

    split store into modules

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,10 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-import DROPBOX_MODULE from "./modules/dropbox.js";
+import files from "./modules/files.js";
 
 export default new Vuex.Store({
     modules: {
-        dropbox: DROPBOX_MODULE
+        files
     }
 });
