ba1ce040c605e8755bc737c9a3dd3e42a0b8bc1f
Matthew Mamonov <g.smellyshovel@gmail.com>
2019-02-08 23:03:15 +0300

    build the tree as a part of the fetching process updateFilesList -> updateFiles

--- src/store/modules/files.js
+++ src/store/modules/files.js
@@ -6,7 +6,9 @@ export default {
         fatalError: null,
         excusableError: null,
         loading: true,
-        filesList: []
+
+        filesList: [],
+        filesTree: {}
     },
     mutations: {
         SET_FATAL_ERROR(state, err) {
@@ -16,6 +18,10 @@ export default {
 
         UPDATE_FILES_LIST(state, filesList) {
             state.filesList = filesList;
+        },
+
+        UPDATE_FILES_TREE(state, filesTree) {
+            state.filesTree = filesTree;
             state.loading = false;
         }
     },
@@ -29,10 +35,13 @@ export default {
             }
         },
 
-        async updateFilesList({ commit }) {
+        async updateFiles({ commit }) {
             try {
                 let filesList = await API.getFilesList();
                 commit("UPDATE_FILES_LIST", filesList);
+
+                let filesTree = API.Helpers.buildTree(filesList);
+                commit("UPDATE_FILES_TREE", filesTree);
             } catch (err) {
                 console.error(err);
                 commit("SET_FATAL_ERROR", err);
@@ -42,12 +51,10 @@ export default {
     getters: {
         fatalError: state => state.fatalError,
         excusableError: state => state.excusableError,
-
         loading: state => state.loading,
 
-        tree: state => {
-            return API.Helpers.buildTree(state.filesList);
-        },
+        filesList: state => state.filesList,
+        filesTree: state => state.filesTree,
 
         folderByPath: (state, getters) => (path = "") => {
             if (path === "") {
