c884478a5cf27ce1abd8b2f96d9cbb344406d77f
Matthew Mamonov <g.smellyshovel@gmail.com>
2019-02-26 18:53:10 +0300

    style body and app (basic)

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,16 @@
 </script>
 
 <style>
+body {
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    box-sizing: border-box;
+    margin: 0;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+}
 </style>
